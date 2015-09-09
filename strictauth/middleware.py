681a88327bb07a31b4eea7ca7cd4f5df8939f6e1
steve yeago <subsume@gmail.com>
2015-09-08 22:12:13 -0400

    Update middleware.py

--- strictauth/middleware.py
+++ strictauth/middleware.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import logout
 class StrictAuthentication:
-	def process_view(self,request,view_func,view_args,view_kwargs):
-		if request.user.is_authenticated() and not request.user.is_active:
-			logout(request)
+   def process_view(self,request,view_func,view_args,view_kwargs):
+      if request.session.accessed and request.user.is_authenticated() and not request.user.is_active:
+         logout(request)
