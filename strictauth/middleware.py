2dd218a1033ca27838d88440096a2e7cd92b5ac6
steve yeago <subsume@gmail.com>
2020-01-29 03:17:11 -0500

    Update middleware.py

--- strictauth/middleware.py
+++ strictauth/middleware.py
@@ -4,5 +4,5 @@ from django.utils.deprecation import MiddlewareMixin
 
 class StrictAuthentication(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
-       if request.session.accessed and request.user.is_authenticated() and not request.user.is_active:
+       if request.session.accessed and request.user.is_authenticated and not request.user.is_active:
           logout(request)
